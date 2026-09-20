8c2ef6a587b7c74c01a37781b99a658c1ee5eaba
zannasalamova389-wq <zannasalamova389@gmail.com>
2026-09-20 11:53:03 +0500

    Обновила файлы кофейни.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const orderButton = document.querySelector('#order');
 const addButtons = document.querySelector('.add');
 let cartCount = 0;
+//Кнопка <<Заказать кофе
+orderButton.addEventListener('click',< function(){
+alert('Спасибо за заказ! Мы свяяжимся с вам.');
+});
 // Кнопки <<В корзину>>
 addButtons.forEach(function(btn) {
 btn.addEventListener('click', function() {
 cartCount = cartCount +1;
-btn.texcontent = 'В корзине ✅ ';
+btn.textContent = 'В корзине ✅ ';
 btn.style.backgroundColor = '#4a2c1a';
 alert('Товар в корзине: ' + cartCount);
 });
