47ae48932dbe5864022f2acdf077db51187c59a0
zannasalamova389-wq <zannasalamova389@gmail.com>
2026-09-23 18:31:07 +0500

    Работает корзина с кнопками  + и -.

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 const orderButton = document.querySelector('#order');
-const addButton = document.querySelectorAll('.add');
 const cartCountSpan = document.querySelector('#cart-count');
-let carCount = 0;
+let cartCount = 0;
 //Кнопка <<Заказать  кофе>>
 orderButton.addEventListener('click', function() {
 alert('Спасибо за заказ! Мы свяжемся с вами.');
 });
-//Кнопки <<В корзину>>
-addButton.forEach(function(btn) {
-btn.addEventListener('click', function() {
-carCount = carCount + 1;
-btn.textContent = 'В корзине ✔️';
-btn.style.backgroundColor = '#4a2c1a';
-btn.style.color = '#fff';
-btn.disabled = true; // блокируем повторные  нажатия
-cartCountSpan.textContent = carCount;
+// Обрабатываем каждую карточку
+const cards = document.querySelectorAll('.card');
+cards.forEach(function(card) {
+const addButton = card.querySelector('.add');
+const counter  = card.querySelector('.counter');
+const minusButton = card.querySelector('.minus');
+const plusButton = card.querySelector('.plus');
+const qtySpan = card.querySelector('.qty');
+let qty =1; // количество для карточки
+// Нажали  <<В корзину>>
+addButton.addEventListener('click', function() {
+addButton.style.display = 'none';   // прячем кнопку
+counter.style.display = 'flex';  // показываем счётчик
+qty = 1;
+qtySpan.textContent = qty;
+cartCount = cartCount + 1;
+cartCountSpan.textContent = cartCount;
 });
+// Нажать <<+>>
+plusButton.addEventListener('click', function() {
+qty = qty + 1;
+qtySpan.textContent = qty;
+cartCount = cartCount + 1;
+cartCountSpan.textContent = cartCount;
+});
+// Нажали <<->>
+minusButton.addEventListener('click', function() {
+if (qty >1) {
+qty = qty -1;
+qtySpan.textContent = qty;
+cartCount = cartCount -1;
+cartCountSpan.textContent = cartCount;
+} else {
+// Если количество 0 - возвращаем кнопку <<В корзину>>
+addButton.style.display = 'inline-block';
+counter.style.display = 'none';
+cartCount = cartCount - 1;
+cartCountSpan.textContent = cartCount;
+     }  
+   });
 });
